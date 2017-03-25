74a16ad59020e4fe12a4375cde488451ec45dd56
叶嘉雨 <jiayu.ye@ele.me>
2017-03-25 20:06:07 +0800

    test: add timeout case

--- test/wait-event.js
+++ test/wait-event.js
@@ -7,7 +7,7 @@ test('wait ready', async (t) => {
   const event = new EventEmitter()
 
   setTimeout(function () {
-    event.emit('readyc', 'result')
+    event.emit('ready', 'result')
   }, 1000);
   const result = await wait(event, 'ready');
   t.is(result, 'result')
@@ -18,11 +18,11 @@ test('wait timeout', async (t) => {
   const event = new EventEmitter()
 
   setTimeout(function () {
-    event.emit('ready', 'result')
-  }, 1000);
+    event.emit('ready')
+  }, 2000);
 
   try {
-    await wait(event, 'ready', 2000);
+    await wait(event, 'ready', 1000);
   } catch (e) {
     t.is(e.name, 'TimeoutError')
   }
